f037392298ab2315af3e2d03239581e391f33720
MuhammadArsyaPradana <muhammadarsyapradana_2327240057@mhs.mdp.ac.id>
2025-12-17 11:09:13 +0700

    add

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+];
+ 
